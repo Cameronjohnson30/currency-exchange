5fba988a2cdab1781a9c911de5a43be12ae01708
cameronjohnson30 <cameron_johnson91@icloud.com>
2022-03-26 20:44:53 -0700

    add class exchangeRate, fetch api, response and error functions

---
+++ src/exchangeRate.js
@@ -0,0 +1,14 @@
+export default class exchangeRate {  
+  static getExchange (location, location2, amount) {
+    return fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/${location}/${location2}/${amount}`)
+      .then(function(response) {
+        if (!response.ok) {
+          throw Error(response.statusText);
+        }
+        return response.json();
+      })
+      .catch(function(error) {  
+        return error
+      });
+  }
+}
