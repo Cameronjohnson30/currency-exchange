829bf3c96417d20dc45e44291296a76cb1bb239b
cameronjohnson30 <cameron_johnson91@icloud.com>
2022-03-26 20:54:51 -0700

    add clear results, getElements, ready, refresh functions

--- src/index.js
+++ src/index.js
@@ -3,3 +3,32 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import exchangeRate from './exchangeRate.js';
+
+function clearResults () {
+  $('.showErrors').text("");
+  $('.showResults').text("");
+}
+
+function getElements(response) {
+  if (response) {
+    $('.showResults').text(`The total exchange ${response.conversion_result}.`);
+  } else {
+    $('.showErrors').text(`There was an error: ${response}`);
+  }
+}
+
+$(document).ready(function() {
+  $('#exchange').click(function() {
+    let location = $('#location').val();
+    let location2 = $('#location2').val();
+    let amount = $('#amount').val();
+    exchangeRate.getExchange(location, location2, amount)
+      .then(function(response) {
+        getElements(response);
+      });
+  });
+});
+
+$('#refresh').click(function() {
+  clearResults();
+});
